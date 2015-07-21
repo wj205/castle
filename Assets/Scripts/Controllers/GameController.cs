23988c832fc15629d07c98faf0b1ada73c127b71
Will Jeffers <wfj205@gmail.com>
2015-07-20 22:27:20 -0400

    Added stupid WinGame and LoseGame functions to a GameController

---
+++ Assets/Scripts/Controllers/GameController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject winText;
+	public GameObject loseText;
+
+	void Start()
+	{
+		winText.SetActive (false);
+		loseText.SetActive (false);
+	}
+
+	void Update()
+	{
+		if(Input.GetKeyDown (KeyCode.Space))
+		{
+			WinGame ();
+		}
+	}
+
+	public void WinGame()
+	{
+		winText.SetActive (true);
+	}
+
+	public void LoseGame()
+	{
+		loseText.SetActive (true);
+	}
+}
